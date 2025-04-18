3173a4f3f5126f95515d95f434fd49e474a15743
imaneboulahya <imaneboulahya705pgmail.com>
2025-04-18 19:44:29 +0200

    commit r

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-(function (c, s, q, u, a, r, e) {
-    c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
-    c._hjSettings = { hjid: a };
-    r = s.getElementsByTagName('head')[0];
-    e = s.createElement('script');
-    e.async = true;
-    e.src = q + c._hjSettings.hjid + u;
-    r.appendChild(e);
-})(window, document, 'https://static.hj.contentsquare.net/c/csq-', '.js', 5362624);
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
